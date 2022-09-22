93e8583c83f11b21bcd41c6e31be7fd3838a2209
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2022-09-22 14:41:54 +0700

    Pengenalan Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Navbar";
+import Header from './Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header /> 
       <Navbar />
       <h2>Rajendra Rakha</h2>
     </div>
